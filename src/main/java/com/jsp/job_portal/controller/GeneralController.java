cf7fd713c186ceb116093366f5432fe5717336d8
saishkulkarni <saishkulkarni7@gmail.com>
2024-12-26 17:50:02 +0530

    admin part done

--- src/main/java/com/jsp/job_portal/controller/GeneralController.java
+++ src/main/java/com/jsp/job_portal/controller/GeneralController.java
@@ -1,14 +1,21 @@
 package com.jsp.job_portal.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.jsp.job_portal.dto.Job;
 import com.jsp.job_portal.dto.JobSeeker;
 import com.jsp.job_portal.dto.Recruiter;
 import com.jsp.job_portal.helper.AES;
+import com.jsp.job_portal.repository.JobRepository;
 import com.jsp.job_portal.repository.JobSeekerRepository;
 import com.jsp.job_portal.repository.RecruiterRepository;
 
@@ -16,6 +23,14 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class GeneralController {
+
+	@Value("${portal.admin.email}")
+	private String adminEmail;
+	@Value("${portal.admin.password}")
+	private String adminPassword;
+	
+	@Autowired
+	JobRepository jobRepository;
 
 	@Autowired
 	JobSeekerRepository jobSeekerRepository;
@@ -56,75 +71,128 @@ public class GeneralController {
 	@PostMapping("/login")
 	public String login(@RequestParam("emph") String emph, @RequestParam("password") String password,
 			HttpSession session) {
-		Long mobile = null;
-		String email = null;
 
-		try {
-			mobile = Long.parseLong(emph);
-			Recruiter recruiter = recruiterRepository.findByMobile(mobile);
-			JobSeeker jobSeeker = jobSeekerRepository.findByMobile(mobile);
-			if (recruiter == null && jobSeeker == null) {
-				session.setAttribute("error", "Invalid Mobile Number");
-				return "redirect:/login";
-			} else {
-				if (recruiter != null) {
-					if (AES.decrypt(recruiter.getPassword()).equals(password)) {
-						session.setAttribute("success", "Login Success as Recruiter");
-						session.setAttribute("recruiter", recruiter);
-						return "redirect:/recruiter/home";
-					} else {
-						session.setAttribute("error", "Invalid Password");
-						return "redirect:/login";
-					}
+		if (emph.equals(adminEmail) && password.equals(adminPassword)) {
+			session.setAttribute("success", "Login Success as Admin");
+			session.setAttribute("admin", "admin");
+			return "redirect:/admin/home";
+		} else {
+			Long mobile = null;
+			String email = null;
+
+			try {
+				mobile = Long.parseLong(emph);
+				Recruiter recruiter = recruiterRepository.findByMobile(mobile);
+				JobSeeker jobSeeker = jobSeekerRepository.findByMobile(mobile);
+				if (recruiter == null && jobSeeker == null) {
+					session.setAttribute("error", "Invalid Mobile Number");
+					return "redirect:/login";
 				} else {
-					if (AES.decrypt(jobSeeker.getPassword()).equals(password)) {
-						session.setAttribute("success", "Login Success as JobSeeker");
-						session.setAttribute("jobSeeker", jobSeeker);
-						return "redirect:/jobseeker/home";
+					if (recruiter != null) {
+						if (AES.decrypt(recruiter.getPassword()).equals(password)) {
+							session.setAttribute("success", "Login Success as Recruiter");
+							session.setAttribute("recruiter", recruiter);
+							return "redirect:/recruiter/home";
+						} else {
+							session.setAttribute("error", "Invalid Password");
+							return "redirect:/login";
+						}
 					} else {
-						session.setAttribute("error", "Invalid Password");
-						return "redirect:/login";
+						if (AES.decrypt(jobSeeker.getPassword()).equals(password)) {
+							session.setAttribute("success", "Login Success as JobSeeker");
+							session.setAttribute("jobSeeker", jobSeeker);
+							return "redirect:/jobseeker/home";
+						} else {
+							session.setAttribute("error", "Invalid Password");
+							return "redirect:/login";
+						}
 					}
 				}
-			}
 
-		} catch (NumberFormatException e) {
-			email = emph;
-			Recruiter recruiter = recruiterRepository.findByEmail(email);
-			JobSeeker jobSeeker = jobSeekerRepository.findByEmail(email);
-			if (recruiter == null && jobSeeker == null) {
-				session.setAttribute("error", "Invalid Email");
-				return "redirect:/login";
-			} else {
-				if (recruiter != null) {
-					if (AES.decrypt(recruiter.getPassword()).equals(password)) {
-						session.setAttribute("success", "Login Success as Recruiter");
-						session.setAttribute("recruiter", recruiter);
-						return "redirect:/recruiter/home";
-					} else {
-						session.setAttribute("error", "Invalid Password");
-						return "redirect:/login";
-					}
+			} catch (NumberFormatException e) {
+				email = emph;
+				Recruiter recruiter = recruiterRepository.findByEmail(email);
+				JobSeeker jobSeeker = jobSeekerRepository.findByEmail(email);
+				if (recruiter == null && jobSeeker == null) {
+					session.setAttribute("error", "Invalid Email");
+					return "redirect:/login";
 				} else {
-					if (AES.decrypt(jobSeeker.getPassword()).equals(password)) {
-						session.setAttribute("success", "Login Success as JobSeeker");
-						session.setAttribute("jobSeeker", jobSeeker);
-						return "redirect:/jobseeker/home";
+					if (recruiter != null) {
+						if (AES.decrypt(recruiter.getPassword()).equals(password)) {
+							session.setAttribute("success", "Login Success as Recruiter");
+							session.setAttribute("recruiter", recruiter);
+							return "redirect:/recruiter/home";
+						} else {
+							session.setAttribute("error", "Invalid Password");
+							return "redirect:/login";
+						}
 					} else {
-						session.setAttribute("error", "Invalid Password");
-						return "redirect:/login";
+						if (AES.decrypt(jobSeeker.getPassword()).equals(password)) {
+							session.setAttribute("success", "Login Success as JobSeeker");
+							session.setAttribute("jobSeeker", jobSeeker);
+							return "redirect:/jobseeker/home";
+						} else {
+							session.setAttribute("error", "Invalid Password");
+							return "redirect:/login";
+						}
 					}
 				}
-			}
 
+			}
 		}
 	}
-	
+
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		session.removeAttribute("jobSeeker");
 		session.removeAttribute("recruiter");
+		session.removeAttribute("admin");
 		session.setAttribute("success", "Logged Out Successfully");
 		return "redirect:/";
+	}
+
+	@GetMapping("/admin/home")
+	public String adminHome(HttpSession session) {
+		if (session.getAttribute("admin") != null)
+			return "admin-home.html";
+		else {
+			session.setAttribute("error", "Invalid Session, Login Again");
+			return "redirect:/login";
+		}
+	}
+
+	@GetMapping("/admin/manage-jobs")
+	public String manageJobs(HttpSession session, ModelMap map) {
+		if (session.getAttribute("admin") != null) {
+			List<Job> jobs=jobRepository.findAll();
+			if(jobs.isEmpty()) {
+				session.setAttribute("failure", "No Jobs Added Yet");
+				return "redirect:/admin/home";
+			}else {
+				map.put("jobs", jobs);
+				return "admin-jobs.html";
+			}
+		} else {
+			session.setAttribute("error", "Invalid Session, Login Again");
+			return "redirect:/login";
+		}
+	}
+	
+	@GetMapping("/admin/change/{id}")
+	public String changeStatus(@PathVariable("id") int id,HttpSession session) {
+		if (session.getAttribute("admin") != null) {
+			Job job=jobRepository.findById(id).orElseThrow();
+			if(job.isApproved())
+				job.setApproved(false);
+			else
+				job.setApproved(true);
+		
+			jobRepository.save(job);
+			session.setAttribute("success", "Status Changed Success");
+			return "redirect:/admin/manage-jobs";
+		}else {
+			session.setAttribute("error", "Invalid Session, Login Again");
+			return "redirect:/login";
+		}
 	}
 }
